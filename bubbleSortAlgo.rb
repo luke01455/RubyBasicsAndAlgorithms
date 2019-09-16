4a66f0f933f3a07f9aeba4925dfb2099c174ec1e
luke01455 <luke0145555@hotmail.co.uk>
2019-09-16 20:15:21 +0100

    sqrt algo and i was wrong about last algo, it did need truth loop

--- bubbleSortAlgo.rb
+++ bubbleSortAlgo.rb
@@ -1,9 +1,9 @@
 
-=begin
+
 def bubble_sort(array)
     n = array.length
     loop do
-        swapped = false  # dont understand why this is needed
+        swapped = false  
 
         (n-1).times do |i|
             if array[i] > array[i+1]
@@ -12,25 +12,10 @@ def bubble_sort(array)
             end
         end
 
-        break if not swapped  # what?
+        break if !swapped 
     end
     
     array  # does this mean return?
 
 end
-
-=end
-
-# refactoring this dudes code
-
-def bubble_sort(array)
-    n = array.length
-    (n-1).times do |i|
-        if array[i] > array[i+1]
-            array[i], array[i+1] = array[i+1], array[i] 
-        end
-    end
-        array
-end
-
-puts bubble_sort([2, 4, 7, 6, 10, 9])
+puts bubble_sort([10, 9, 7, 5, 1, 2])
---
+++ perfectSquareAlgo.rb
@@ -0,0 +1,14 @@
+def perfect_square(num)
+
+s_root = Math.sqrt(num)
+
+    if s_root % 1 == 0
+        new_square = s_root + 1
+    new_square * new_square
+    else
+     -1
+
+    end
+end
+
+puts perfect_square(155)
